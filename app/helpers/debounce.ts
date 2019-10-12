7a8a554d42571e75b991a7341823484f985e8174
Dominic Claßen <dominic@dominic-classen.de>
2019-10-12 17:55:36 +0200

    add source of debounce function

--- app/helpers/debounce.ts
+++ app/helpers/debounce.ts
@@ -1,3 +1,5 @@
+// https://davidwalsh.name/javascript-debounce-function (2019/10/11)
+
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
