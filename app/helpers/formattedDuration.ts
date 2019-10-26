1c1e84ab1929d118fdc16a42d85e94527d88c90a
Dominic Claßen <dominic@dominic-classen.de>
2019-10-26 23:58:03 +0200

    fix: correct calculation of formattedDuration

--- app/helpers/formattedDuration.ts
+++ app/helpers/formattedDuration.ts
@@ -3,9 +3,10 @@ const formattedDuration = (duration: number) => {
     return (new Array(length + 1).join(pad) + value).slice(-length);
   };
 
-  const hours = duration / 3600;
-  const minutes = Math.floor(duration / 60);
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration - hours * 3600) / 60);
   const seconds = duration - minutes * 60;
+
   const finalTime =
     (hours >= 1 ? str_pad_left(hours, "0", 2) + ":" : "") +
     str_pad_left(minutes, "0", 2) +
