430f49c5462f2c2fd8456133f31fc0cecc13ce95
Dominic Claßen <dominic.classen@coduct.io>
2019-10-30 00:13:43 +0100

    chore: remove dummy settings

--- app/containers/AppSettingsPage.tsx
+++ app/containers/AppSettingsPage.tsx
@@ -1,9 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import Switch from "react-switch";
 import styled from "styled-components";
 import CustomSwitch from "../components/Customs/CustomSwitch/CustomSwitch";
-import Divider from "../components/Divider/Divider";
 import { RootStoreModel } from "../dataLayer/stores/RootStore";
 import useInject from "../hooks/useInject";
 
@@ -37,56 +35,6 @@ const AccountPage: React.FunctionComponent = () => {
       <Header>App Settings</Header>
       <SettingsContainer>
         <CustomSwitch label="Use DiscordRPC" onChange={_switchRPCStatus} checked={app.rpcEnabled} />
-        <Divider size={3} />
-        <label
-          style={{
-            display: "flex",
-            alignItems: "center",
-            width: "200px",
-            justifyContent: "space-between"
-          }}
-        >
-          <Switch
-            onChange={() => console.log("LDL")}
-            checked={app.rpcEnabled}
-            onColor="#99ccff"
-            onHandleColor="#565f6c"
-            handleDiameter={30}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-            height={20}
-            width={48}
-          />
-          <span>Test</span>
-        </label>
-        <Divider size={3} />
-        <label
-          style={{
-            display: "flex",
-            alignItems: "center",
-            width: "200px",
-            justifyContent: "space-between"
-          }}
-        >
-          <Switch
-            onChange={() => console.log("LDL")}
-            checked={app.rpcEnabled}
-            onColor="#99ccff"
-            onHandleColor="#565f6c"
-            handleDiameter={30}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-            height={20}
-            width={48}
-          />
-          <span>Test 2</span>
-        </label>
-        <Divider size={3} />
-
       </SettingsContainer>
     </Container>
   );
