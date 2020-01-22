c6453b9da27c2ab588789b27429bf705a5aa22ac
majesnix <dominic.classen@coduct.io>
2020-01-22 11:35:04 +0100

    fix: lag when song is moved in playlist

--- app/dataLayer/models/Playlist.ts
+++ app/dataLayer/models/Playlist.ts
@@ -176,10 +176,10 @@ export default class Playlist extends Model({
     try {
       const track = this.tracks[oldIndex].current;
 
-      yield* _await(ApiClient.moveTrackTo(this.id, track.id, newIndex));
-
       this.tracks.splice(oldIndex, 1);
       this.tracks.splice(newIndex, 0, trackRef(track));
+
+      yield* _await(ApiClient.moveTrackTo(this.id, track.id, newIndex));
     } catch (error) {
       AyeLogger.player(
         `Error changing Track order ${JSON.stringify(error, null, 2)}`,
