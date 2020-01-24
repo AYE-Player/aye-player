a5d39ed379f263fdee619be907ef7d92a184b191
majesnix <dominic.classen@coduct.io>
2020-01-24 19:39:31 +0100

    chore: remove useless observer

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,6 +1,5 @@
 import ListIcon from "@material-ui/icons/List";
 import SearchIcon from "@material-ui/icons/Search";
-import { observer } from "mobx-react-lite";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
@@ -74,4 +73,4 @@ const Navigation: React.FunctionComponent<any> = props => {
   );
 };
 
-export default observer(Navigation);
+export default Navigation;
