51d701bf63fa741b5d272c5e0ecc6545c7e3b3ff
majesnix <dominic.classen@coduct.io>
2020-12-19 15:44:02 +0100

    chore: remove var

--- app/helpers/removeControlCharacters.ts
+++ app/helpers/removeControlCharacters.ts
@@ -2,7 +2,7 @@ const removeControlCharacters = (str: string) => {
   return str
     .split("")
     .filter(x => {
-      var n = x.charCodeAt(0);
+      const n = x.charCodeAt(0);
 
       return 31 < n && 127 > n;
     })
