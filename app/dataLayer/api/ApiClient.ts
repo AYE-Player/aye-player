66722e80f2948ca220551d64685e43f824949a8f
majesnix <dominic@dominic-classen.de>
2021-04-01 08:48:15 +0200

    fix: ky import

--- app/dataLayer/api/ApiClient.ts
+++ app/dataLayer/api/ApiClient.ts
@@ -3,7 +3,7 @@
  * https://graphql.org/learn/best-practices/
  */
 
-import ky from "ky/umd";
+import ky from "ky";
 import { IUserInfoDto } from "../../types/response";
 import Track from "../models/Track";
 import {
